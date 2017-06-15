dddc13949dc480dbedca1f063c2e302e15d0b2d1
ffiargus <rm.chow@outlook.com>
2017-06-15 20:02:33 +0000

    changes vh to 98

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ class App extends Component {
 
   render() {
     return (
-      <div style ={{width:'100%', height: '400px'}}>
+      <div style ={{width:'100%', height: '98vh'}}>
       <GoogleMapReact
         defaultCenter={this.state.center}
         defaultZoom={this.state.zoom}

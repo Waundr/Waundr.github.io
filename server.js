f8876b356f4d9734ef7c096bffd3bce1a249b5bd
ffiargus <rm.chow@outlook.com>
2017-06-19 15:05:42 +0000

    puts ws into main server file

--- server.js
+++ server.js
@@ -5,11 +5,16 @@ var config = require('./webpack.config');
 var redis = require('redis');
 var client = redis.createClient();
 
+const express = require('express');
+const SocketServer = require('ws').Server;
+
+let events = [];
+
 client.on('connect', function() {
     console.log('connected');
 });
 
-new WebpackDevServer(webpack(config), {
+server = new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     watchOptions: {
       aggregateTimeout: 300,
@@ -24,3 +29,45 @@ new WebpackDevServer(webpack(config), {
 
     console.log('Running at http://0.0.0.0:3000');
   });
+
+
+
+const wss = new SocketServer({ server });
+
+// Set up a callback that will run when a client connects to the server
+// When a client connects they are assigned a socket, represented by
+// the ws parameter in the callback.
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  const broadcast = (message) => {
+
+    console.log("broadcast is called")
+    wss.clients.forEach((c) => {
+      if(c != ws) {
+        c.send(JSON.stringify(message));
+      }
+    });
+  }
+
+  events.forEach((event) => {
+    ws.send(JSON.stringify(message));
+  })
+
+
+  ws.on('message', function incoming(message) {
+    let newMarker = JSON.parse(message);
+    console.log(newMarker);
+    events.push(newMarker);
+    client.hmset(`event${events.length - 1}`, newMarker)
+    broadcast(message)
+    client.hgetall(`event${events.length - 1}`, (err, obj) => {
+      console.log(obj)
+    })
+
+  })
+
+ // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+  ws.on('close', () => console.log('Client disconnected'));
+});
+

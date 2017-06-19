868f9cf682ab1ed3bc9edabe80736aed205d80d4
Arjun <lallarjun@gmail.com>
2017-06-19 22:20:42 +0000

    added marker icon mapped to event type

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,7 @@ class App extends Component {
         lat={marker.loc.lat}
         lng={marker.loc.lng} className="material-icons"
         key={index}>
-        room
+        {this.typeToIcon(marker.type)}
       </div>
     ));
       //google map react component takes in center/zoom/options/onchange settings
@@ -180,6 +180,26 @@ class App extends Component {
     // this.setState({markers: markers})
     // console.log(marker,index)
 
+  }
+
+  typeToIcon = (type) => {
+
+    switch (type) {
+      case "Food Stand":
+        return "restaurant"
+      case "Street Market":
+        return "store"
+      case "Entertainment":
+        return "casino"
+      case "Meet up":
+        return "group_add"
+      case "Obstacle":
+        return "nature_people"
+      case "Your friends":
+        return "people"
+      default:
+        return "room"
+    }
   }
 }
 

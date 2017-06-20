21cb0af46882d34d7b0ce83ea22c1acaa1c0d257
ffiargus <rm.chow@outlook.com>
2017-06-20 20:37:24 +0000

    sends markers through get request instead of sockets

--- production/server.js
+++ production/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 
 const redis = require('redis');
@@ -20,23 +21,28 @@ client.flushdb( function (err, succeeded) {
     console.log("flushing redis.."+ succeeded); // will be true if successfull
 });
 
+app.use(cors());
 
 app.get('/', function(req, res) {
     console.log('home')
     res.send('hellow')
 })
 
-app.get('/events', (req, res) => {
+app.get('/events.json', (req, res) => {
+  console.log('3', events)
+
   res.json(events)
 })
 
 
-app.use(express.static('public'))
+server = app.use(express.static('public'))
    .use("/users", usersRoutes()) //routes for handling user logins
    .use("/markers", markerRoutes(client)) //markers needs redis client
    .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
-const wss = new SocketServer({ server: app });
+const wss = new SocketServer({ server });
+
+console.log('1', events)
 
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
@@ -65,6 +71,8 @@ wss.on('connection', (ws) => {
     delete newMarker.loc;
     console.log(newMarker);
     events.push(newMarker);
+    console.log('2', events)
+
     client.hmset(`event${events.length - 1}`, newMarker)
     broadcast('update markers')
     client.hgetall(`event${events.length - 1}`, (err, obj) => {

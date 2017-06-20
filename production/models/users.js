f5edc706549da1cfa395a3ab37ed7c85aa4e4bd6
Arjun <lallarjun@gmail.com>
2017-06-20 21:51:31 +0000

    setup models

--- production/models/users.js
+++ production/models/users.js
@@ -1,11 +1,16 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var Users = sequelize.define('Users', {
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
+    allowNull: false
   }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+        //         User.hasMany(models.freinds?, {
+        //   foreignKey: 'todoId',
+        //   as: 'todoItems',
+        // });
       }
     }
   });

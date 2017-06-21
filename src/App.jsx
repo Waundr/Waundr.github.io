d6100e16c4d2814c0ef6183b6f9592ddd548cb92
promilo <milind.shah@mail.utoronto.ca>
2017-06-21 20:54:21 +0000

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -89,7 +89,7 @@ class App extends Component {
     const Markers = this.state.markers.map((marker, index) => (
       <div className="marker" onClick={() => this.onClick(marker)}
         lat={marker.loc.lat}
-        lng={marker.loc.lng} className="material-icons md-48" style={{fontSize: "50px"}}
+        lng={marker.loc.lng} className="material-icons md-48" style={{fontSize: "30px"}}
         key={index}>
         {this.typeToIcon(marker.type)}
       </div>
--- src/RegisterModal.jsx
+++ src/RegisterModal.jsx
@@ -15,15 +15,20 @@ class RegisterModal extends Component{
             <Input ref='password' label="Password"/>
             <Input ref='confirm_password' label="Confirm Password"/>
           </Row>
-          <Button className="modal-action modal-close btn waves-effect waves-light" type="submit">Register
+          <Button className="modal-action modal-close btn waves-effect waves-light" type="submit" onClick={this._onRegister.bind(this)} >Register
             <Icon className="material-icons right">send</Icon>
           </Button>
         </Modal>
 
       )
+
+      }
+      _onRegister = (e) => {
+        console.log(this.refs)
+        let email = this.refs.email_id.state.value
+        let pass = this.refs.password.state.value
+        let conf = this.refs.confirm_password.state.value
+        console.log("New Register", email, pass, conf)
     }
-  }
-
-
-
-        export default RegisterModal
+  };
+export default RegisterModal

83dffc6ea6952d2dff4af91b6e6149af0518a736
promilo <milind.shah@mail.utoronto.ca>
2017-06-22 23:42:36 +0000

    started doing jwt

--- src/index.jsx
+++ src/index.jsx
@@ -7,5 +7,16 @@ require("../styles/application.scss");
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+var store = createStore(
+  (state = {}) => state,
+  applyMiddleware(thunk)
+)
+
+ReactDOM.render(
+  <Provide store ={store}>
+  <App />
+  </Provider>, document.getElementById('react-root'));

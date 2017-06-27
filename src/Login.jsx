a86f98f81c35c57d3bc1bbcbba1a29f3eeb24f86
Arjun <lallarjun@gmail.com>
2017-06-27 16:25:20 +0000

    new login page, new buttons, new query

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,11 +3,22 @@ import { Modal, Button, Row, Input, Icon, Col} from 'react-materialize';
 
 class Login extends Component{
 
+
+
   render(){
       return(
         <div>
-          <a href='http://localhost:3001/users/auth/google'><Button waves ='light' className="blue-grey darken-3" style = {{color: "#FFD074"}}> Login with google </Button></a>
-          <a href='http://localhost:3001/users/auth/facebook'><Button waves ='light' className="blue-grey darken-3" style = {{color: "#FFD074"}}> Login with facebook </Button></a>
+          <main id='main' className='main_area'>
+            <div className='center'>
+              <h1>Go Waundr</h1>
+              <button className="loginBtn loginBtn--google">
+                <a href='http://localhost:3001/users/auth/google'>Login with Google</a>
+              </button>
+              <button className="loginBtn loginBtn--facebook">
+                <a href='http://localhost:3001/users/auth/facebook'>Login with Facebook</a>
+              </button>
+            </div>
+          </main>
         </div>
       )
     }

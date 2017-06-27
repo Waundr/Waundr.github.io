f2002f505c3bfc21dc05f53ec303dc7239c9f539
promilo <milind.shah@mail.utoronto.ca>
2017-06-27 16:58:03 +0000

    Added button color toggles to the infowindow

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ class App extends Component {
   };
 
   componentDidMount() {
-    // FETCH CALL
+    // FETCH CALL for thel ogin
     fetch("http://localhost:3001/users", {credentials: 'include', mode: 'cors', 'Access-Control-Allow-Credentials': true })
     .then((promise) => {
       promise.json().then((user) => {
@@ -68,10 +68,6 @@ class App extends Component {
 
         })
       })
-
-
-
-
 
     //initiate connection to WS server
     const ws = new WebSocket("ws://localhost:3001");
@@ -176,7 +172,7 @@ class App extends Component {
 
           {Markers}
       </GoogleMapReact>
-      <div id ='filter' className="fixed-action-btn horizontal click-to-toggle" style={{position: 'absolute', bottom: '8em', right: '2.65em'}}  >
+      <div id ='filter' className="fixed-action-btn horizontal click-to-toggle" style={{position: 'absolute', bottom: '8em', right: '2em'}}  >
         <a className="btn-floating btn-large waves-effect waves-light blue-grey darken-3" >
           <i className="material-icons" style = {{color: "#FD8F04"}}>filter_list</i>
         </a>
@@ -305,8 +301,8 @@ class App extends Component {
     this.infowindow = new google.maps.InfoWindow({
 
       content: "<div id='iw-container'>" + "<div class='iw-title'>"+ marker.title +"</div>" + "<br /><div class='iw-content'>" + marker.description +  "</div><div class='confirmbtns'>" +
-      "<a onclick='$.ajax({url: `http://localhost:3001/events`, method: `POST`, data: {id:`"+ marker.id + "`, user:`"+ this.state.id + "`, confirm:`confirm`}, success: (data)=>{if (data === `plus`){let num = parseInt($(`.markercount`).text(), 10);num++;$(`.markercount`).text(num)}else if(data === `minus`){let num = parseInt($(`.markercount`).text(), 10);num--;$(`.markercount`).text(num)}},failure: ()=>{console.log(`confirm failed`)}})' class='btn-floating blue-grey darken-3'><p class='markercount'>" + marker.confirms.length + "</p></a>" +
-      "<a onclick='$.ajax({url: `http://localhost:3001/events`, method: `POST`, data: {id:`"+ marker.id + "`, user:`"+ this.state.id + "`, confirm:`reject`}, success: (data)=>{console.log(`worked!`)},failure: ()=>{console.log(`reject failed`)}})' class='btn-floating blue-grey darken-3'><i class='material-icons' style='color:#FD8F04'>clear</i></a>" + "<br/>" + "</div></div>",
+      "<a onclick='$.ajax({url: `http://localhost:3001/events`, method: `POST`, data: {id:`"+ marker.id + "`, user:`"+ this.state.id + "`, confirm:`confirm`}, success: (data)=>{if (data === `plus`){let num = parseInt($(`.markercount`).text(), 10);num++;$(`.markercount`).text(num);$(`#" + marker.id +"count`).removeClass(`blue-grey darken-3`)}else if(data === `minus`){let num = parseInt($(`.markercount`).text(), 10);num--;$(`.markercount`).text(num);$(`#" + marker.id +"count`).addClass(`blue-grey darken-3`)}},failure: ()=>{console.log(`confirm failed`)}})' id='"+marker.id+"count' class='btn-floating red blue-grey darken-3'><p class='markercount'>" + marker.confirms.length + "</p></a>" +
+      "<a onclick='$.ajax({url: `http://localhost:3001/events`, method: `POST`, data: {id:`"+ marker.id + "`, user:`"+ this.state.id + "`, confirm:`reject`}, success: (data)=>{console.log(`worked!`); $(`#" + marker.id +"clear`).removeClass(`blue-grey darken-3`)},failure: ()=>{console.log(`reject failed`)}})' id='"+marker.id+"clear'class='btn-floating blue-grey darken-3'><i class='material-icons' style='color:#FD8F04'>clear</i></a>" + "<br/>" + "</div></div>",
       position: marker.loc
 
     });

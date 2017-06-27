9ea50026a2f44ad4081880d03d1be0040c872941
ffiargus <rm.chow@outlook.com>
2017-06-27 02:54:17 +0000

    markers with hardcoded json

--- Veronica/Waundr/src/app.js
+++ Veronica/Waundr/src/app.js
@@ -2,13 +2,23 @@
 
 import React, { Component } from 'react';
 import MapView from 'react-native-maps';
+import {events} from './data';
 
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Dimensions
 } from 'react-native';
+
+const {width, height} = Dimensions.get('window')
+
+const SCREEN_HEIGHT = height;
+const SCREEN_WIDTH = width;
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 export default class Waundr extends Component {
 constructor(props) {
@@ -27,6 +37,44 @@ constructor(props) {
 
 onRegionChange(region) {
   this.setState({ region });
+};
+
+componentDidMount = () => {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      var lat = parseFloat(position.coords.latitude)
+      var lng = parseFloat(position.coords.longitude)
+
+      var initialRegion = {
+        latitude: lat,
+        longitude: lng,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      }
+      this.setState({initialPosition: initialRegion});
+      this.setState({markerPosition: initialRegion});
+    },
+    (error) => alert(error.message),
+    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+  );
+  this.watchID = navigator.geolocation.watchPosition((position) => {
+    var lat = parseFloat(position.coords.latitude)
+    var lng = parseFloat(position.coords.longitude)
+
+    var lastRegion = {
+      latitude: lat,
+      longitude: lng,
+      latitudeDelta: LATITUDE_DELTA,
+      longitudeDelta: LONGITUDE_DELTA
+    }
+
+    this.setState({initialPosition: lastRegion});
+    this.setState({markerPosition: lastRegion});
+  });
+}
+
+componentWillUnmount() {
+  navigator.geolocation.clearWatch(this.watchID)
 }
 
 render() {
@@ -36,10 +84,29 @@ render() {
      // provider={PROVIDER_GOOGLE}
     initialRegion={this.state.region}
       onRegionChange={this.onRegionChange.bind(this)}
-  />
-  </View>
+  >
+    {events.map((event, index) =>
+      <MapView.Marker
+        coordinate={{latitude: event.lat,
+                    longitude: event.lng}}
+        title={event.title}
+        description={event.description}/>
+        )}
+
+      <MapView.Marker
+        coordinate={this.state.markerPosition}
+        >
+        <View style={styles.radius}>
+          <View style={styles.marker}/>
+        </View>
+      </MapView.Marker>
+      </MapView>
+    </View>
   );
 }
+
+
+
 }
 
 const styles = StyleSheet.create({
@@ -60,6 +127,23 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  radius: {
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+    backgroundColor: 'rgba(0, 122, 255, 0.2)',
+    borderWidth: 1,
+    borderColor: 'rgba(0, 122, 255, 0.4)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  marker: {
+    height: 10,
+    width: 10,
+    borderRadius: 5,
+    backgroundColor: 'skyblue',
+
   },
 });
 

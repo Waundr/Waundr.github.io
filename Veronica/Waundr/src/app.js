7d0be5f51e22ecd4233987fbec09d98d8536b56d
ffiargus <rm.chow@outlook.com>
2017-06-27 04:31:07 +0000

    ios app pulls event information from server

--- Veronica/Waundr/src/app.js
+++ Veronica/Waundr/src/app.js
@@ -1,18 +1,25 @@
 
 
 import React, { Component } from 'react';
+import { Navigator } from 'react-native-deprecated-custom-components';
+import Events from './events';
 import MapView from 'react-native-maps';
-import {events} from './data';
+// import { events } from './data';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { apiMiddleware, reducer } from './redux';
 
 import {
+  ActivityIndicator,
+  RefreshControl,
   AppRegistry,
   StyleSheet,
   Text,
   View,
-  Dimensions
+  Dimensions,
 } from 'react-native';
 
-const {width, height} = Dimensions.get('window')
+const { width, height } = Dimensions.get('window')
 
 const SCREEN_HEIGHT = height;
 const SCREEN_WIDTH = width;
@@ -20,132 +27,26 @@ const ASPECT_RATIO = width / height;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
-export default class Waundr extends Component {
-constructor(props) {
-  super(props);
-  this.state = {
-    region: {
-      latitude: 43.644546,
-      longitude: -79.395170,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    },
+const store = createStore(reducer, {}, applyMiddleware(apiMiddleware));
 
-  }
-  };
+store.dispatch({type: 'GET_EVENT_DATA'});
 
-
-onRegionChange(region) {
-  this.setState({ region });
+const RouteMapper = (route, navigator) => {
+  return <Events navigator={navigator} />;
 };
 
-componentDidMount = () => {
-  navigator.geolocation.getCurrentPosition(
-    (position) => {
-      var lat = parseFloat(position.coords.latitude)
-      var lng = parseFloat(position.coords.longitude)
-
-      var initialRegion = {
-        latitude: lat,
-        longitude: lng,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA
-      }
-      this.setState({initialPosition: initialRegion});
-      this.setState({markerPosition: initialRegion});
-    },
-    (error) => alert(error.message),
-    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
-  );
-  this.watchID = navigator.geolocation.watchPosition((position) => {
-    var lat = parseFloat(position.coords.latitude)
-    var lng = parseFloat(position.coords.longitude)
-
-    var lastRegion = {
-      latitude: lat,
-      longitude: lng,
-      latitudeDelta: LATITUDE_DELTA,
-      longitudeDelta: LONGITUDE_DELTA
-    }
-
-    this.setState({initialPosition: lastRegion});
-    this.setState({markerPosition: lastRegion});
-  });
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Navigator
+          initialRoute={{ name: 'events' }}
+          configureScene={(route, routeStack) => Navigator.SceneConfigs.FloatFromBottom}
+          renderScene={RouteMapper}
+        />
+      </Provider>
+    );
+  }
 }
-
-componentWillUnmount() {
-  navigator.geolocation.clearWatch(this.watchID)
-}
-
-render() {
-  return (
-    <View style={styles.container}>
-     <MapView style={styles.map}
-     // provider={PROVIDER_GOOGLE}
-    initialRegion={this.state.region}
-      onRegionChange={this.onRegionChange.bind(this)}
-  >
-    {events.map((event, index) =>
-      <MapView.Marker
-        coordinate={{latitude: event.lat,
-                    longitude: event.lng}}
-        title={event.title}
-        description={event.description}/>
-        )}
-
-      <MapView.Marker
-        coordinate={this.state.markerPosition}
-        >
-        <View style={styles.radius}>
-          <View style={styles.marker}/>
-        </View>
-      </MapView.Marker>
-      </MapView>
-    </View>
-  );
-}
-
-
-
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  map: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  radius: {
-    height: 40,
-    width: 40,
-    borderRadius: 20,
-    backgroundColor: 'rgba(0, 122, 255, 0.2)',
-    borderWidth: 1,
-    borderColor: 'rgba(0, 122, 255, 0.4)',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  marker: {
-    height: 10,
-    width: 10,
-    borderRadius: 5,
-    backgroundColor: 'skyblue',
-
-  },
-});
-
 
 
---
+++ Veronica/Waundr/src/redux.js
@@ -0,0 +1,49 @@
+import { Platform } from 'react-native';
+
+const API = 'http://localhost:3001/mobile/events.json';
+
+export const apiMiddleware = store => next => action => {
+  // Pass all actions through by default
+  next(action);
+  switch (action.type) {
+    // In case we receive an action to send an API request
+    case 'GET_EVENT_DATA':
+      // Dispatch GET_MOVIE_DATA_LOADING to update loading state
+      store.dispatch({type: 'GET_EVENT_DATA_LOADING'});
+      // Make API call and dispatch appropriate actions when done
+      fetch(API)
+        .then(response => response.json())
+        .then(data => next({
+          type: 'GET_EVENT_DATA_RECEIVED',
+          data
+        }))
+        .catch(error => next({
+          type: 'GET_EVENT_DATA_ERROR',
+          error
+        }));
+      break;
+    // Do nothing if the action does not interest us
+    default:
+      break;
+  }
+};
+
+export const reducer = (state = { events: [], loading: true }, action) => {
+  switch (action.type) {
+    case 'GET_EVENT_DATA_LOADING':
+      return {
+        ...state,                   // keep the existing state,
+        loading: true,              // but change loading to true
+      };
+    case 'GET_EVENT_DATA_RECEIVED':
+      console.log('action data here',action.data.events)
+      return {
+        loading: false,             // set loading to false
+        events: action.data.events, // update movies array with reponse data
+      };
+    case 'GET_EVENT_DATA_ERROR':
+      return state;
+    default:
+      return state;
+    }
+};

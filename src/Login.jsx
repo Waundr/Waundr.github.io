36c89799a204d48f2e66ad35da4284d1c1558040
promilo <milind.shah@mail.utoronto.ca>
2017-06-28 17:37:56 +0000

    Added some styling and improved code readability

--- src/Login.jsx
+++ src/Login.jsx
@@ -47,19 +47,18 @@ class Login extends Component{
             <div className='center'>
               <div style={{'backgroundColor':'black'}}>
               <img src="../styles/Waundr-logo (wht).png"/>
-              <div style={{'marginTop': '-4em'}}>
-              <h4>The real time social walking experience</h4>
-              <h5 style={{'color':'white'}} >Discover {this.state.discover}</h5>
-              </div>
-              <div style={{'margin-top':'2em'}}>
-              <button className="loginBtn loginBtn--google">
-                <a href='http://localhost:3001/users/auth/google'>Login with Google</a>
-              </button>
-              <button className="loginBtn loginBtn--facebook">
-                <a href='http://localhost:3001/users/auth/facebook'>Login with Facebook</a>
-              </button>
-              </div>
-
+                <div style={{'marginTop': '-4em'}}>
+                  <h4>The real time social walking experience</h4>
+                  <h5 style={{'color':'white'}} >Discover {this.state.discover}</h5>
+                </div>
+                <div style={{'margin-top':'2em'}}>
+                  <button className="loginBtn loginBtn--google">
+                    <a href='http://localhost:3001/users/auth/google'>Login with Google</a>
+                  </button>
+                  <button className="loginBtn loginBtn--facebook">
+                    <a href='http://localhost:3001/users/auth/facebook'>Login with Facebook</a>
+                  </button>
+                </div>
               </div>
             </div>
           </main>

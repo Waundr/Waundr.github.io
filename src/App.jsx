8778f032e948538c904f50d6a4dba5f39b4e41c8
ffiargus <rm.chow@outlook.com>
2017-06-29 15:50:57 +0000

    fixes typo and build

--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,7 @@ class App extends Component {
     function error(err) {
       console.log(`ERROR - ${err.message}`)
     }
-    navigator.geolocation.getCurrentPostion(setLocation, error, options)
+    navigator.geolocation.getCurrentPosition(setLocation, error, options)
   }
 
   componentDidMount() {
